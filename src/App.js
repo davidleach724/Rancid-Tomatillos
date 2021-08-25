a2a6b43c9f0004b5c6e14f6a5a64471181af6d12
David Leach <davidleach724@gmail.com>
2021-08-25 14:49:58 -0600

    Remove unneeded code in index and App files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import movieData from './movieData'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      
+    }
+  }
+
+
+  render() {
+    return(
+      <h1>Test {console.log(movieData)}</h1>
+    )
+  }
 }
 
-export default App;
+export default App
