906a524b928303ffface6dace49a9548137092c4
David Leach <davidleach724@gmail.com>
2021-08-25 15:19:13 -0600

    Create Card and Movie Component files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
 import movieData from './movieData'
+import Card from './Card'
+import Movies from './Movies'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      
+      movies: { movieData }
     }
   }
 
-
   render() {
     return(
-      <h1>Test {console.log(movieData)}</h1>
+      <main>
+        <h1>Header Goes Here</h1>
+        {console.log(this.state.movies.movieData)}
+        <Card />
+      </main>
     )
   }
 }
---
+++ src/Card.js
@@ -0,0 +1,6 @@
+import react from 'react'
+import './Card.css'
+
+
+
+export default Card
