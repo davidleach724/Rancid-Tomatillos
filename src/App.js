baa2038b1904c2eeed159fdd254e6f0a1f3511dc
David Leach <davidleach724@gmail.com>
2021-08-26 15:06:16 -0600

    Create chooseMovie function and pass props

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react'
 import movieData from './movieData'
 import Movies from './Movies'
+import SingleMovie from './SingleMovie'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      movies: { movieData }
+      movies: { movieData },
+      singleMovie: []
     }
+  }
+
+  chooseMovie = (id) => {
+    const chosenMovie = this.state.movies.movieData.find(movie => movie.id === id)
+    this.setState({ singleMovie: [chosenMovie]})
   }
 
   render() {
     return(
       <main>
         <h1>🍅 🤢 Rancid Tomatillos</h1>
-        <Movies movies={this.state.movies}/>
+        {(this.state.singleMovie.length > 1) && 
+          <SingleMovie {...this.state.singleMovie}/>}
+        {(this.state.singleMovie.length < 1) && 
+          <Movies movies={this.state.movies} chooseMovie={this.chooseMovie}/>}
+        
       </main>
     )
   }
---
+++ src/SingleMovie.js
@@ -0,0 +1,10 @@
+import react from 'react'
+import './SingleMovie.css'
+
+const SingleMovie = () => {
+  return (
+    <h1>Single Movie Right Here Yes Sir</h1>
+  )
+}
+
+export default SingleMovie
