42158a7c3e9504cdc3cc16612c3838108542a307
David Leach <davidleach724@gmail.com>
2021-08-26 18:04:25 -0600

    Create left and right side

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -4,9 +4,14 @@ import './SingleMovie.css'
 const SingleMovie = ({ singleMovie, clearMovie }) => {
   return (
     <div className="single-movie-container" 
-    style={{ backgroundImage: `url(${singleMovie.backdrop_path})` }}>
-      <h2>{singleMovie.title}</h2>
-      <p></p>
+    style={{ backgroundImage: `url(${singleMovie.backdrop_path}), url(https://www.solidbackgrounds.com/images/1920x1080/1920x1080-black-solid-color-background.jpg)` }}>
+      <div className="left-side">
+        <h2>{singleMovie.title}</h2>
+
+      </div>
+      <div className="right-side">
+        
+      </div>
       <button onClick={() => clearMovie()}>GO BACK</button>
     </div>
   )
