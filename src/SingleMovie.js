66450a2e78a296fa159d68e3d9f3f123a89f0189
David Leach <davidleach724@gmail.com>
2021-08-26 19:35:22 -0600

    Add items to display movie info

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -5,14 +5,23 @@ const SingleMovie = ({ singleMovie, clearMovie }) => {
   return (
     <div className="single-movie-container" 
     style={{ backgroundImage: `url(${singleMovie.backdrop_path}), url(https://www.solidbackgrounds.com/images/1920x1080/1920x1080-black-solid-color-background.jpg)` }}>
-      <div className="left-side">
-        <h2>{singleMovie.title}</h2>
-
+      <div className="movie-information">
+        <div className="left-side">
+          <h2>{singleMovie.title}</h2>
+          <img className= "movie-info-poster" src={singleMovie.poster_path}></img>
+          <button onClick={() => clearMovie()}>GO BACK</button>
+        </div>
+        <div className="right-side">
+          <h4>"Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!"</h4>
+          <h5>"It's a movie!"</h5>
+          <p>Release Date: </p>
+          <p>Run Time: </p>
+          <p>Genres: </p>
+          <p>Avg. Rating: </p>
+          <p>Budget: </p>
+          <p>Revenue: </p>
+        </div>
       </div>
-      <div className="right-side">
-        
-      </div>
-      <button onClick={() => clearMovie()}>GO BACK</button>
     </div>
   )
 }
