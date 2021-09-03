0dc7f6c611408bbd378ce5f318fe9df4d21c2f9b
Samantha Brown <bro7445@gmail.com>
2021-09-02 20:26:35 -0400

    Create test for Navbar to contain website title

--- cypress/integration/movie_home_page.js
+++ cypress/integration/movie_home_page.js
@@ -3,3 +3,7 @@ describe('User movie flows', () => {
   beforeEach(() => {
       cy.visit('http://localhost:3000')
     })
+
+    it('Navbar should contain website title', () => {
+        cy.get('h1').contain('🍅 🤢 Rancid Tomatillos 🎥')
+    })
